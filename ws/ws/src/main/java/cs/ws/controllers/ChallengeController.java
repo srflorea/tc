cd26c18aa95a457d7e5771edfbaa66c8a97b83dd
Stefan Razvan Florea <srflorea22@gmail.com>
2016-01-22 16:27:34 -0500

    Added web service eclipse project

---
+++ ws/ws/src/main/java/cs/ws/controllers/ChallengeController.java
@@ -0,0 +1,30 @@
+package cs.ws.controllers;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cs.ws.models.Challenge;
+import cs.ws.utils.HibernateUtils;
+
+@RestController
+public class ChallengeController {
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping("/challenge")
+	public Challenge challenge() {
+		
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		Criteria criteria = session.createCriteria(Challenge.class);
+		criteria.add(Restrictions.eq(Challenge.Fields.PROJECT_ID.getName(), 6680l));
+		List<Challenge> challenges = criteria.list();
+		
+		return new Challenge(challenges.size(), "challenges");
+	}
+}
